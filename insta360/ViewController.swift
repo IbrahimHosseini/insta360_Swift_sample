598ee2127f08d1525d246f4d7d51d705d2f9d59e
Ibrahim Hosseini <sehosseini@me.com>
2022-12-19 10:18:15 +0330

    Initial Commit

---
+++ insta360/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  insta360
+//
+//  Created by Ibrahim on 12/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
